025a672f5c97987fb19eba2dea75dd90d60f2e84
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-04 15:15:04 -0600

    Creates preliminary project structure.

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+import tensorflow
+
+
+def assert_gpu_available():
+    session = tensorflow.Session(config=tensorflow.ConfigProto(log_device_placement=True))
+    device_types = [device.device_type for device in session.list_devices()]
+    assert 'GPU' in device_types, 'GPU not available in devices: {}'.format(device_types)
