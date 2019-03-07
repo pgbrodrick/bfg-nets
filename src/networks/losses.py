c9a37abb70c8fe7e68edf672df75c07bb7b9a595
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-07 10:22:33 -0600

    Deletes duplicated loss function.

--- src/networks/losses.py
+++ src/networks/losses.py
@@ -1,18 +1,6 @@
 import keras
 
 
-# TODO: - delete if you're okay with the update
-# def cropped_mse(loss_buffer_constant):
-#    def _cropped_mse(y_true, y_pred):
-#        # Need to reference y_pred shape, y_true shape will be tuple of Nones during compilation
-#        buffer_x = int(loss_buffer_constant * keras.backend.int_shape(y_pred)[1])
-#        buffer_y = int(loss_buffer_constant * keras.backend.int_shape(y_pred)[2])
-#        y_true = y_true[:, buffer_x:-buffer_x, buffer_y:-buffer_y, :]
-#        y_pred = y_pred[:, buffer_x:-buffer_x, buffer_y:-buffer_y, :]
-#        return keras.losses.mean_squared_error(y_true, y_pred)
-#    return _cropped_mse
-
-
 def cropped_loss(outer_width, inner_width, loss_type, weighted=False):
     """ Loss function with optional per-pixel weighting
         and edge trimming options.
