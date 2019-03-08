0d3dd2d396cc0652b5dd125f5e04b1dfeb5a949f
pbrodrick <pbrodrick@carnegiescience.edu>
2019-03-07 18:06:07 -0800

    partway through thinking through scaling, migrating machines

--- src/data_management/__init__.py
+++ src/data_management/__init__.py
@@ -49,9 +49,12 @@ class DataConfig:
         # A flag to fill in missing data with a nearest neighbor interpolation.
         self.fill_in_feature_data = kwargs.get('fill_in_feature_data', False)
 
-        # TODO: rename/revise these as appropriate
-        self.global_scaling = kwargs.get('global_scaling', None)
-        self.local_scaling = kwargs.get('local_scaling', None)
+        # Local and global feature and response scaling
+        self.local_feature_scaling = kwargs.get('local_feature_scaling', None)
+        self.local_response_scaling = kwargs.get('local_response_scaling', None)
+
+        self.global_feature_scaling = kwargs.get('global_feature_scaling', None)
+        self.global_response_scaling = kwargs.get('global_response_scaling', None)
 
         # A random seed to set (for reproducability), set to None to not set a seed.
         self.random_seed = kwargs.get('random_seed', 13)
