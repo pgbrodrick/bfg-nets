e8b31fef8a9a0e3b00ee5d1937f20b88e7fedd03
pbrodrick_pc <phil.brodrick@gmail.com>
2019-03-11 14:57:21 -0700

    small syntax changes

--- src/data_management/apply_model_to_data.py
+++ src/data_management/apply_model_to_data.py
@@ -1,23 +1,13 @@
 import gdal
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
 
 from src.util.general import *
 
 
-
-def apply_semantic_segmentation(input_file_list,\
-                                output_folder,\
-                                model,\
-                                window_radius,\
-                                application_name='',
-                                internal_window_radius=None,\
-                                make_png=True,\
-                                make_tif=True,\
-                                local_scale_flag=None,\
-                                global_scale_file=None,\
-                                png_dpi=200,\
-                                verbose=False,
-                                nodata_value=-9999):
 
 def apply_model_to_raster(cnn, data_config, feature_file, destination_basename, make_png=False, make_tif=True, feature_transformer=None, response_transformer=None)
     """ Apply a trained model to a raster file.
@@ -53,12 +43,12 @@ def apply_model_to_raster(cnn, data_config, feature_file, destination_basename, 
       feature[:,:,n] = dataset.GetRasterBand(n+1).ReadAsArray()
   
     if (not dataset.GetRasterBand(1).GetNoDataValue() is None):
-      feature[feature == dataset.GetRasterBand(1).GetNoDataValue()] = data_config.feature_nodata_value
+        feature[feature == dataset.GetRasterBand(1).GetNoDataValue()] = data_config.feature_nodata_value
     feature[np.isnan(feature)] = data_config.feature_nodata_value
     feature[np.isinf(feature)] = data_config.feature_nodata_value
   
     if (feature_transformer is not None):
-      feature = feature_transformer(feature)
+        feature = feature_transformer(feature)
 
     output = np.zeros((feature.shape[0],feature.shape[1], cnn.n_classes)) + data_config.response_nodata_value
    
@@ -71,62 +61,62 @@ def apply_model_to_raster(cnn, data_config, feature_file, destination_basename, 
     rowlist.append(rr[1]-data_config.window_radius)
   
     for col in collist:
-      images = []
-      for n in rowlist:
-        d = feature[n-data_config.window_radius:n+data_config.window_radius,col-data_config.window_radius:col+data_config.window_radius].copy()
-        if(d.shape[0] == data_config.window_radius*2 and d.shape[1] == data_config.window_radius*2):
-          # TODO: implement local scaling if necessary
-
-          # TODO: consider having this as an option
-          #d = fill_nearest_neighbor(d) 
-          images.append(d)
-        images = np.stack(images)
-        images = images.reshape((images.shape[0],images.shape[1],images.shape[2],dataset.RasterCount))
-      
-        pred_y = model.predict(images)
-        
-        _i = 0
+        images = []
         for n in rowlist:
-          p = pred_y[_i,...]
-          if (data_config.internal_window_radius < data_config.window_radius):
-            mm = rint(data_config.window_radius - data_config.internal_window_radius)
-            p = p[mm:-mm,mm:-mm,:]
-          output[n-data_config.internal_window_radius:n+data_config.internal_window_radius,col-data_config.internal_window_radius:col+data_config.internal_window_radius,:] = p
-          _i += 1
-          if (_i >= len(images)):
-            break
-      
-      # TODO: think through if this order of operations screws things up if response_nodata_value != feature_nodata_value
-      output[np.all(feature == data_config.feature_nodata_value,axis=-1),:] = data_config.response_nodata_value
-      if (feature_transformer is not None):
-        output = feature_transformer.inverse_transform(output)
-      
-      if (make_png):
-        output[output == data_config.response_nodata_value] = np.nan
-        feature[feature == data_config.response_nodata_value] = np.nan
-        gs1 = gridspec.GridSpec(1,cnn.n_classes+1)
-        for n in range(0,cnn.n_classes):
-          ax = plt.subplot(gs1[0,n])
-          im = plt.imshow(output[:,:,n],vmin=0,vmax=1)
-          plt.axis('off')
+            d = feature[n-data_config.window_radius:n+data_config.window_radius,col-data_config.window_radius:col+data_config.window_radius].copy()
+            if(d.shape[0] == data_config.window_radius*2 and d.shape[1] == data_config.window_radius*2):
+                # TODO: implement local scaling if necessary
+
+                # TODO: consider having this as an option
+                #d = fill_nearest_neighbor(d)
+                images.append(d)
+            images = np.stack(images)
+            images = images.reshape((images.shape[0],images.shape[1],images.shape[2],dataset.RasterCount))
+
+            pred_y = model.predict(images)
+
+            _i = 0
+            for n in rowlist:
+              p = pred_y[_i,...]
+              if (data_config.internal_window_radius < data_config.window_radius):
+                  mm = int(round(data_config.window_radius - data_config.internal_window_radius))
+                  p = p[mm:-mm,mm:-mm,:]
+              output[n-data_config.internal_window_radius:n+data_config.internal_window_radius,col-data_config.internal_window_radius:col+data_config.internal_window_radius,:] = p
+              _i += 1
+              if (_i >= len(images)):
+                  break
+
+        # TODO: think through if this order of operations screws things up if response_nodata_value != feature_nodata_value
+        output[np.all(feature == data_config.feature_nodata_value,axis=-1),:] = data_config.response_nodata_value
+        if (feature_transformer is not None):
+            output = feature_transformer.inverse_transform(output)
+
+        if (make_png):
+            output[output == data_config.response_nodata_value] = np.nan
+            feature[feature == data_config.response_nodata_value] = np.nan
+            gs1 = gridspec.GridSpec(1,cnn.n_classes+1)
+            for n in range(0,cnn.n_classes):
+                ax = plt.subplot(gs1[0,n])
+                im = plt.imshow(output[:,:,n], vmin=0, vmax=1)
+                plt.axis('off')
   
-        ax = plt.subplot(gs1[0,cnn.n_classes])
-        im = plt.imshow(np.squeeze(feature[...,0]))
-        plt.axis('off')
-        plt.savefig(destination_basename + '.png',dpi=png_dpi,bbox_inches='tight')
-        plt.clf()
-        
-      if (make_tif):
-        driver = gdal.GetDriverByName('GTiff') 
-        driver.Register()
-        output[np.isnan(output)] = nodata_value
-         
-        outDataset = driver.Create(destination_basename + '.tif',output.shape[1],output.shape[0],cnn.n_classes,gdal.GDT_Float32)
-        outDataset.SetProjection(dataset.GetProjection())
-        outDataset.SetGeoTransform(dataset.GetGeoTransform())
-        for n in range(0,cnn.n_classes):
-          outDataset.GetRasterBand(n+1).WriteArray(np.squeeze(output[:,:,n]),0,0)
-        del outDataset
-      del dataset
+            ax = plt.subplot(gs1[0,cnn.n_classes])
+            im = plt.imshow(np.squeeze(feature[...,0]))
+            plt.axis('off')
+            plt.savefig(destination_basename + '.png', dpi=png_dpi, bbox_inches='tight')
+            plt.clf()
+
+        if (make_tif):
+            driver = gdal.GetDriverByName('GTiff')
+            driver.Register()
+            output[np.isnan(output)] = data_config.response_nodata_value
+
+            outDataset = driver.Create(destination_basename + '.tif',output.shape[1], output.shape[0], cnn.n_classes, gdal.GDT_Float32)
+            outDataset.SetProjection(dataset.GetProjection())
+            outDataset.SetGeoTransform(dataset.GetGeoTransform())
+            for n in range(0,cnn.n_classes):
+                outDataset.GetRasterBand(n+1).WriteArray(np.squeeze(output[:,:,n]),0,0)
+            del outDataset
+        del dataset
  
 
