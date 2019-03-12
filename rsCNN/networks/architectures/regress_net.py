7d628861b35b09dae526c664b6bad0f0bea3c1e7
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-12 09:35:00 -0500

    Adds typing, todos, clarity to regress_net script.

--- rsCNN/networks/architectures/regress_net.py
+++ rsCNN/networks/architectures/regress_net.py
@@ -1,48 +1,55 @@
-import keras
+from typing import Iterable
 
+import keras
 from keras.layers.convolutional import Conv2D
 from keras.layers.normalization import BatchNormalization
 
 
-def flat_regress_net(inshape, n_classes, conv_depth, batch_norm, n_layers, conv_pattern, output_activation):
+# TODO:  Convert to kwargs with default settings matching those of NetworkConfig? Would help by giving examples of
+#  reasonable or expected values, allow use to call function easily without setting config (i.e., fewer params), allow
+#  user to provide params in different order, etc.
+def flat_regress_net(
+    inshape: Iterable[int],
+    n_classes: int,
+    conv_depth: int,
+    batch_norm: bool,
+    n_layers: int,
+    conv_pattern: Iterable[int],
+    output_activation: keras.layers.Activation,
+) -> keras.models.Model:
     """ Construct a flat style network with flexible shape
 
     Arguments:
     inshape - tuple/list
       Designates the input shape of an image to be passed to
       the network.
+      # TODO:  Do we want to enforce a particular number of dimensions?
     n_classes - int
       The number of classes the network is meant to regress
     kwargs - dict
       A dictionary of optional keyword arguments, which may contain
       extra keywords.  Values to use are:
-
       conv_depth - int/str
         If integer, a fixed number of convolution filters to use
         in the network.  If 'growth' tells the network to grow
         in depth to maintain a constant number of neurons.
+        # TODO:  Is growth the default if integers are not supplied? If so, maybe we say None == growth functionality?
       batch_norm - bool
         Whether or not to use batch normalization after each layer.
+    # TODO:  info for conv_pattern, currently unclear
 
     Returns:
       A flexible flat style network keras network.
     """
     if (len(conv_pattern) > 0):
-        if (n_layers % len(conv_pattern) != 0):
-            Exception('conv_pattern must divide into n_layers')
-            quit()
-
-    if (isinstance(conv_depth, int) == False):
-        Exception('conv_depth parameter must be an integer')
-        quit()
+        assert (n_layers % len(conv_pattern) == 0), 'conv_pattern must divide into n_layers'
 
     inlayer = keras.layers.Input(inshape)
     b1 = inlayer
     for i in range(n_layers):
-        b1 = Conv2D(conv_depth, (conv_pattern[i % len(conv_pattern)], conv_pattern[i %
-                                                                                   len(conv_pattern)]), activation='relu', padding='same')(b1)
-        b1 = Conv2D(conv_depth, (conv_pattern[i % len(conv_pattern)], conv_pattern[i %
-                                                                                   len(conv_pattern)]), activation='relu', padding='same')(b1)
+        kernel_size = conv_pattern[i % len(conv_pattern)]
+        b1 = Conv2D(conv_depth, (kernel_size, kernel_size), activation='relu', padding='same')(b1)
+        b1 = Conv2D(conv_depth, (kernel_size, kernel_size), activation='relu', padding='same')(b1)
         if (batch_norm):
             b1 = BatchNormalization()(b1)
 
