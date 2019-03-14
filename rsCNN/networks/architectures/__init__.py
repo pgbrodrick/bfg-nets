7e5eb0fd33751f69231bc37d93f70476d7d0ba4a
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-14 13:32:43 -0500

    Changes architecture templates to use 'architecture_options' key.

--- rsCNN/networks/architectures/__init__.py
+++ rsCNN/networks/architectures/__init__.py
@@ -9,7 +9,7 @@ def create_config_file_from_architecture_defaults():
     for arch_module in (regress_net, residual_net, residual_unet, unet):
         module_name = arch_module.__name__.split('.')[-1]
         config = configparser.ConfigParser()
-        config[module_name] = arch_module.parse_architecture_options(**dict())
+        config['architecture_options'] = arch_module.parse_architecture_options(**dict())
         with open(os.path.join(DIR_TEMPLATES, module_name + '.ini'), 'w') as file_:
             config.write(file_)
 
