52778a1f90e859c0e88d32dcd27b22cdd9c39810
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-15 13:25:30 -0500

    Updates unet to use standard format, arbitrarily keeps residuals separate.

--- rsCNN/networks/architectures/unet.py
+++ rsCNN/networks/architectures/unet.py
@@ -1,31 +1,53 @@
+from typing import Tuple
+
 import keras
-
-from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D
-from keras.layers.normalization import BatchNormalization
+from keras.layers import BatchNormalization, Concatenate, Conv2D, MaxPooling2D, UpSampling2D
 
 
+DEFAULT_BLOCK_STRUCTURE = (2, 2, 2, 2)
 DEFAULT_BATCH_NORM = True
 DEFAULT_INITIAL_FILTERS = 64
+DEFAULT_KERNEL_SIZE = (3, 3)
+DEFAULT_MIN_CONV_WIDTH = 8
+DEFAULT_PADDING = 'same'
+DEFAULT_POOL_SIZE = (2, 2)
+DEFAULT_STRIDES = (1, 1)
 
 
 def parse_architecture_options(**kwargs):
     return {
+        'block_structure': kwargs.get('block_structure', DEFAULT_BLOCK_STRUCTURE),
         'batch_norm': kwargs.get('batch_norm', DEFAULT_BATCH_NORM),
-        'conv_depth': kwargs.get('conv_depth', DEFAULT_INITIAL_FILTERS),
+        'initial_filters': kwargs.get('initial_filters', DEFAULT_INITIAL_FILTERS),
+        'kernel_size': kwargs.get('kernel_size', DEFAULT_KERNEL_SIZE),
+        'min_conv_width': kwargs.get('min_conv_width', DEFAULT_MIN_CONV_WIDTH),
+        'padding': kwargs.get('padding', DEFAULT_PADDING),
+        'pool_size': kwargs.get('pool_size', DEFAULT_POOL_SIZE),
     }
 
 
-# TODO:  typing and kwargs with defaults
-def create_model(inshape, n_classes, conv_depth, batch_norm, output_activation):
+def create_model(
+        input_shape: Tuple[int, int, int],
+        num_outputs: int,
+        output_activation: str,
+        block_structure: Tuple[int, ...] = DEFAULT_BLOCK_STRUCTURE,
+        batch_norm: bool = DEFAULT_BATCH_NORM,
+        initial_filters: int = DEFAULT_INITIAL_FILTERS,
+        kernel_size: Tuple[int, int] = DEFAULT_KERNEL_SIZE,
+        min_conv_width: int = DEFAULT_MIN_CONV_WIDTH,
+        padding: str = DEFAULT_PADDING,
+        pool_size: Tuple[int, int] = DEFAULT_POOL_SIZE,
+        use_growth: bool = False,
+):
     """ Construct a U-net style network with flexible shape
 
     Arguments:
-    inshape - tuple/list
+    input_shape - tuple/list
       Designates the input shape of an image to be passed to
       the network.
     n_classes - int
       The number of classes the network is meant to classify.
-    conv_depth - int/str
+    filters - int/str
       If integer, a fixed number of convolution filters to use
       in the network.  If 'growth' tells the network to grow
       in depth to maintain a constant number of neurons.
@@ -35,69 +57,65 @@ def create_model(inshape, n_classes, conv_depth, batch_norm, output_activation):
     Returns:
       A U-net style network keras network.
     """
-    inlayer = keras.layers.Input(inshape)
-    growth_flag = False
-    if (conv_depth == 'growth'):
-        growth_flag = True
-        conv_depth = 8
+    input_width = input_shape[0]
+    minimum_width = input_width / 2 ** len(block_structure)
+    assert minimum_width > min_conv_width, \
+        'The convolution width in the last encoding block ({}) is less than '.format(minimum_width) + \
+        'the minimum specified width ({}). Either reduce '.format(min_conv_width) + \
+        'the number of blocks in block_structure (currently {}) or '.format(len(block_structure)) + \
+        'the value of min_conv_width.'
 
-    # get width
-    width = inshape[1]
+    # Need to track the following throughout the model creation
+    filters = initial_filters
+    layers_pass_through = list()
 
-    pool_list = []
-    pre_pool_list = []
-    b1 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(inlayer)
+    # Encodings
+    input_layer = keras.layers.Input(shape=input_shape)
+    encoder = input_layer
+    # Each encoder block has a number of subblocks
+    for num_subblocks in block_structure:
+        for idx_sublayer in range(num_subblocks):
+            # Each subblock has two convolutions
+            encoder = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(encoder)
+            if (batch_norm):
+                encoder = BatchNormalization()(encoder)
+            encoder = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(encoder)
+            if (batch_norm):
+                encoder = BatchNormalization()(encoder)
+        # Each encoder block passes its pre-pooled layers through to the decoder
+        layers_pass_through.append(encoder)
+        encoder = MaxPooling2D(pool_size=pool_size)(encoder)
+        if use_growth:
+            filters *= 2
 
-    # encoding layers
+    # Decodings
+    decoder = encoder
+    # Each decoder block has a number of subblocks, but in reverse order of encoder
+    for num_subblocks, layer_passed_through in zip(reversed(block_structure), reversed(layers_pass_through)):
+        for idx_sublayer in range(num_subblocks):
+            # Each subblock has two convolutions
+            decoder = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(decoder)
+            if (batch_norm):
+                decoder = BatchNormalization()(decoder)
+            decoder = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(decoder)
+            if (batch_norm):
+                decoder = BatchNormalization()(decoder)
+        decoder = UpSampling2D(size=pool_size)(decoder)
+        decoder = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(decoder)
+        if (batch_norm):
+            decoder = BatchNormalization()(decoder)
+        decoder = Concatenate()([layer_passed_through, decoder])
+        if use_growth:
+            filters /= 2
+
+    # Last convolutions
+    output_layer = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(decoder)
     if (batch_norm):
-        b1 = BatchNormalization()(b1)
-    pre_pool_list.append(Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(b1))
-    pool_list.append(MaxPooling2D(pool_size=(2, 2))(pre_pool_list[-1]))
+        output_layer = BatchNormalization()(output_layer)
+    output_layer = Conv2D(filters=filters, kernel_size=kernel_size, padding=padding)(output_layer)
     if (batch_norm):
-        pool_list.append(BatchNormalization()(pool_list[-1]))
-    if(growth_flag):
-        conv_depth = int(2*conv_depth)
+        output_layer = BatchNormalization()(output_layer)
+    output_layer = Conv2D(
+        filters=num_outputs, kernel_size=(1, 1), padding='same', activation=output_activation)(output_layer)
+    return keras.models.Model(inputs=[input_layer], outputs=[output_layer])
 
-    n_encode = 1
-    while width > 8:
-        b2 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(pool_list[-1])
-        if (batch_norm):
-            b2 = BatchNormalization()(b2)
-        pre_pool_list.append(Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(b2))
-        pool_list.append(MaxPooling2D(pool_size=(2, 2))(pre_pool_list[-1]))
-        if (batch_norm):
-            pool_list.append(BatchNormalization()(pool_list[-1]))
-        n_encode += 1
-        width = int(round(width / 2.))
-        if(growth_flag):
-            conv_depth = int(2*conv_depth)
-
-    # decoding layers
-    last_layer = pool_list[-1]
-    for n in range(0, n_encode):
-
-        b2 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(last_layer)
-        if (batch_norm):
-            b2 = BatchNormalization()(b2)
-        b2 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(b2)
-        if (batch_norm):
-            b2 = BatchNormalization()(b2)
-
-        u1 = UpSampling2D(size=(2, 2))(b2)
-        u1 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(u1)
-        if (batch_norm):
-            u1 = BatchNormalization()(u1)
-        last_layer = keras.layers.Concatenate()([pre_pool_list[n_encode-1-n], u1])
-        if(growth_flag):
-            conv_depth = int(conv_depth/2)
-
-    e1 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(last_layer)
-    if (batch_norm):
-        e1 = BatchNormalization()(e1)
-    e2 = Conv2D(conv_depth, (3, 3), activation='relu', padding='same')(e1)
-    if (batch_norm):
-        e2 = BatchNormalization()(e2)
-
-    output_layer = Conv2D(n_classes, (1, 1), activation=output_activation, padding='same')(e2)
-    model = keras.models.Model(input=inlayer, output=output_layer)
-    return model
