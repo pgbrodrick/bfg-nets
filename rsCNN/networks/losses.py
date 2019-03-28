ede21eb81374f6975401b7bd2d9e6818686522a5
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-28 15:05:04 -0500

    Refactors loss function.

--- rsCNN/networks/losses.py
+++ rsCNN/networks/losses.py
@@ -25,25 +25,29 @@ def cropped_loss(loss_type, outer_width, inner_width, weighted=True):
     """
 
     def _cropped_loss(y_true, y_pred):
+        if weighted:
+            weights = y_true[..., -1]
+            y_true = y_true[..., :-1]
+
         if (outer_width != inner_width):
             buffer = int(round((outer_width-inner_width) / 2))
             y_true = y_true[:, buffer:-buffer, buffer:-buffer, :]
             y_pred = y_pred[:, buffer:-buffer, buffer:-buffer, :]
 
         if (loss_type == 'cc' or loss_type == 'categorical_crossentropy'):
-            loss = keras.backend.categorical_crossentropy(y_true[..., :-1], y_pred)
+            loss = keras.backend.categorical_crossentropy(y_true, y_pred)
         elif (loss_type == 'mae' or loss_type == 'mean_absolute_error'):
-            loss = keras.backend.mean(keras.backend.abs(y_true[..., :-1] - y_pred))
+            loss = keras.backend.mean(keras.backend.abs(y_true - y_pred))
         elif (loss_type == 'mse' or loss_type == 'mean_squared_error'):
-            loss = keras.backend.mean(keras.backend.pow(y_true[..., :-1] - y_pred, 2))
+            loss = keras.backend.mean(keras.backend.pow(y_true - y_pred, 2))
         elif (loss_type == 'rmse' or loss_type == 'root_mean_squared_eror'):
-            loss = keras.backend.pow(keras.backend.mean(keras.backend.pow(y_true[..., :-1]-y_pred, 2)), 0.5)
+            loss = keras.backend.pow(keras.backend.mean(keras.backend.pow(y_true - y_pred, 2)), 0.5)
         else:
             raise NotImplementedError('Unknown loss function')
 
         # TODO: check that this after the fact weight multiplication works properly
-        if (weighted):
-            loss = loss * y_true[..., -1]
+        if weighted:
+            loss = loss * weights
 
         return loss
     return _cropped_loss
