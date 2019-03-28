a49f51da460fb6ba91158038b7198314c039d320
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-28 09:49:38 -0500

    Splits Sequence into base and memmapped classes.

--- rsCNN/data_management/sequences.py
+++ rsCNN/data_management/sequences.py
@@ -13,16 +13,68 @@ _logger = logger.get_child_logger(__name__)
 
 class Sequence(keras.utils.Sequence):
     batches_per_epoch = None
+    feature_scaler = None
+    response_scaler = None
+
+    def __len__(self):
+        # Method is required for Keras functionality, a.k.a. steps_per_epoch in fit_generator
+        raise NotImplementedError
+
+    def __getitem__(self, index: int, apply_transforms: bool = True) -> Tuple[np.array, np.array]:
+        # Method is required for Keras functionality
+        raise NotImplementedError
+
+    def get_transformed_batch(self, idx_batch: int):
+        # Method is used for visualizations
+        return self.__getitem__(idx_batch, apply_transforms=True)
+
+    def get_untransformed_batch(self, idx_batch: int):
+        # Method is used for visualizations
+        return self.__getitem__(idx_batch, apply_transforms=False)
+
+    def set_feature_scaler(self, feature_scaler: BaseGlobalScaler):
+        self.feature_scaler = feature_scaler
+
+    def set_response_scaler(self, response_scaler: BaseGlobalScaler):
+        self.response_scaler = response_scaler
+
+    def _scale_features(self, features: np.array) -> np.array:
+        return self.feature_scaler.transform(features)
+
+    def _scale_responses(self, responses: np.array) -> np.array:
+        return self.response_scaler.transform(responses)
+
+    def _apply_random_transformations(self, features: np.array, responses: np.array) -> Tuple[np.array, np.array]:
+        # TODO:  Are random transformations generally useful? How can we incorporate these in a more general way
+        #  without adding additional configuration options? Do we have a training and a testing sequence, and training
+        #  sequences apply these types of transformations by default? Do we have a single config option to introduce
+        #  randomness into the training dataset? Other? This is not currently hooked up.
+        # Flip top to bottom
+        if random.random() > 0.5:
+            features = np.flip(features, axis=0)
+            responses = np.flip(responses, axis=0)
+        # Flip side to side
+        if random.random() > 0.5:
+            features = np.flip(features, axis=1)
+            responses = np.flip(responses, axis=1)
+        # Rotate 0, 1, 2, or 3 times
+        num_rotations = np.floor(4 * random.random())
+        features = np.rot90(features, k=num_rotations, axes=(0, 1))
+        responses = np.rot90(responses, k=num_rotations, axes=(0, 1))
+        return features, responses
+
+
+class MemmappedSequence(Sequence):
+    batches_per_epoch = None
 
     def __init__(
             self,
             features,
             responses,
             weights,
-            batch_size: int,
             feature_scaler: BaseGlobalScaler,
             response_scaler: BaseGlobalScaler,
-            apply_random_transformations: bool = False
+            batch_size: int,
     ) -> None:
         self.features = features
         self.responses = responses
@@ -30,7 +82,6 @@ class Sequence(keras.utils.Sequence):
         self.batch_size = batch_size
         self.feature_scaler = feature_scaler
         self.response_scaler = response_scaler
-        self.apply_random_transformations = apply_random_transformations
 
         self.cum_samples_per_fold = np.zeros(len(features)+1).astype(int)
         for fold in range(1, len(features)+1):
@@ -74,37 +125,8 @@ class Sequence(keras.utils.Sequence):
                                         sample_index:stop_ind, ...], axis=0)
             batch_weights = np.append(batch_weights, (self.weights[current_fold])[sample_index:stop_ind, ...], axis=0)
 
-        batch_features, batch_responses = self._transform_features_and_responses(batch_features, batch_responses)
+        batch_features = self._scale_features(batch_features)
+        batch_responses = self._scale_responses(batch_responses)
         batch_responses = np.append(batch_responses, batch_weights, axis=-1)
 
         return batch_features, batch_responses
-
-    def get_transformed_batch(self, idx_batch):
-        return self.__getitem__(idx_batch, apply_transforms=True)
-
-    def get_untransformed_batch(self, idx_batch):
-        return self.__getitem__(idx_batch, apply_transforms=False)
-
-    def _transform_features_and_responses(self, features: np.array, responses: np.array) -> Tuple[np.array, np.array]:
-        # Required transforms for data quality
-        if (self.feature_scaler is not None):
-            features = self.feature_scaler.transform(features)
-        if (self.response_scaler is not None):
-            responses = self.response_scaler.transform(responses)
-        # Return without transformations if testing and not training
-        if not self.apply_random_transformations:
-            return features, responses
-        # Optional transforms for training variety
-        # Flip top to bottom
-        if random.random() > 0.5:
-            features = np.flip(features, axis=0)
-            responses = np.flip(responses, axis=0)
-        # Flip side to side
-        if random.random() > 0.5:
-            features = np.flip(features, axis=1)
-            responses = np.flip(responses, axis=1)
-        # Rotate 0, 1, 2, or 3 times
-        num_rotations = np.floor(4 * random.random())
-        features = np.rot90(features, k=num_rotations, axes=(0, 1))
-        responses = np.rot90(responses, k=num_rotations, axes=(0, 1))
-        return features, responses
