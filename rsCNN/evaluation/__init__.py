1462fa6b07d3c14f5c98d3cdfa02de7e969e9f10
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-03-28 10:45:56 -0500

    Adds notes and todos.

--- rsCNN/evaluation/__init__.py
+++ rsCNN/evaluation/__init__.py
@@ -22,10 +22,14 @@ class ExperimentReport(object):
             self.experiment.network_config['model']['model_name'] + '.pdf'
         )
         with PdfPages(filepath_report) as pdf:
+            # Model summary
             pdf.savefig(networks.print_model_summary(self.experiment.model))
+            # Model history
             pdf.savefig(networks.plot_history(self.experiment.history))
+            # Input examples and their scaled representations
             for fig in rs_data.plot_raw_and_scaled_input_examples(self.experiment.test_sequence):
                 pdf.savefig(fig)
+            # Output examples and their scaled representations
             for fig in results.plot_predictions(self.experiment.test_sequence, self.experiment):
                 pdf.savefig(fig)
             # TODO:  migrate the last reporting functions here
@@ -33,32 +37,26 @@ class ExperimentReport(object):
 
 def generate_eval_report(cnn, features, responses, weights, fold_assignments, verification_fold, feature_transform, response_transform, data_config):
 
-    visual_stitching_artifact_check = True
-    quant_stitching_artificat_check = True
+    # TODO: migrate these functions above
+    # TODO: convert to lined graphs (borrow code from watershed work)
+    if (prediction_histogram_comp):
+        figs = plot_prediction_histograms(responses.copy(),
+                                          weights.copy(),
+                                          cnn.predict(feature_transform.transform(features)),
+                                          fold_assignments,
+                                          verification_fold,
+                                          response_transform,
+                                          data_config.response_nodata_value)
+        for fig in figs:
+            pdf.savefig(fig)
 
-    n_examples = 10
-
-    with PdfPages(report_name) as pdf:
-
-        # TODO: convert to lined graphs (borrow code from watershed work)
-        if (prediction_histogram_comp):
-            figs = plot_prediction_histograms(responses.copy(),
-                                              weights.copy(),
-                                              cnn.predict(feature_transform.transform(features)),
-                                              fold_assignments,
-                                              verification_fold,
-                                              response_transform,
-                                              data_config.response_nodata_value)
-            for fig in figs:
-                pdf.savefig(fig)
-
-        if (spatial_error_concentration):
-            figs = spatial_error(responses.copy(),
-                                 response_transform.inverse_transform(
-                                     cnn.predict(feature_transform.transform(features))),
-                                 weights.copy(),
-                                 data_config.response_nodata_value)
-            for fig in figs:
-                pdf.savefig(fig)
-        # if (visual_stitching_artifact_check):
-        # if (quant_stitching_artificat_check):
+    if (spatial_error_concentration):
+        figs = spatial_error(responses.copy(),
+                             response_transform.inverse_transform(
+                                 cnn.predict(feature_transform.transform(features))),
+                             weights.copy(),
+                             data_config.response_nodata_value)
+        for fig in figs:
+            pdf.savefig(fig)
+    # if (visual_stitching_artifact_check):
+    # if (quant_stitching_artificat_check):
