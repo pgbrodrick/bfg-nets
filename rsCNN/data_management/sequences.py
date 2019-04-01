e51d616547b359ee1c1cdcac0b3614db92dec196
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-04-01 13:12:57 -0500

    Removes todos, updates names, removes unused methods.

--- rsCNN/data_management/sequences.py
+++ rsCNN/data_management/sequences.py
@@ -12,29 +12,28 @@ _logger = logger.get_child_logger(__name__)
 
 
 class BaseSequence(keras.utils.Sequence):
-    batches_per_epoch = None
     feature_scaler = None
     response_scaler = None
+    apply_random_transforms = None
 
-    def __init__(self, feature_scaler: BaseGlobalScaler, response_scaler: BaseGlobalScaler):
+    def __init__(
+            self,
+            feature_scaler: BaseGlobalScaler,
+            response_scaler: BaseGlobalScaler,
+            batch_size: int,
+            apply_random_transforms: bool = False
+    ) -> None:
         self.feature_scaler = feature_scaler
         self.response_scaler = response_scaler
+        self.batch_size = batch_size
+        self.apply_random_transforms = apply_random_transforms
 
-    def __len__(self):
+    def __len__(self) -> int:
         # Method is required for Keras functionality, a.k.a. steps_per_epoch in fit_generator
         raise NotImplementedError
 
-    # TODO:  Phil:  can we rename apply_transforms to apply_random_transforms for clarity?
-    # Yeah, I think that's safe in the current setup.  Might want to think about also doing the random
-    # transform based on the config.
-    def __getitem__(self, index: int, apply_transforms: bool = True) -> Tuple[np.array, np.array]:
+    def __getitem__(self, index: int) -> Tuple[np.array, np.array]:
         # Method is required for Keras functionality
-        # TODO:  Phil:  I'm trying to make it so we can build our own sequences more easily, even so we can extend the
-        #  memmapped sequence to do things like add noise or other modifications. This is a first-pass at that. I think
-        #  this general strategy makes it possible because we can more flexibly implement submethods when necessary
-        # rather than rewriting __getitem__ every time. Let me know what you think.
-
-        # Seems fine - we're still rewriting get_features_responses_weights every time, you're just centralizing the scaling/transformations?  I can't think of any reason top of head that would cause an issue.
         _logger.debug('Get batch {} with {} items via sequence'.format(index, self.batch_size))
         _logger.debug('Get features, responses, and weights')
         features, responses, weights = self._get_features_responses_weights(index)
@@ -47,17 +46,10 @@ class BaseSequence(keras.utils.Sequence):
         responses = self._scale_responses(responses)
         _logger.debug('Append weights to responses for loss functions')
         responses_with_weights = np.append(responses, weights, axis=-1)
-        if apply_transforms is True:
+        if self.apply_random_transforms is True:
             _logger.debug('Apply random transformations to features and responses')
             self._apply_random_transformations(features, responses)
-
-    def get_transformed_batch(self, idx_batch: int):
-        # Method is used for visualizations
-        return self.__getitem__(idx_batch, apply_transforms=True)
-
-    def get_untransformed_batch(self, idx_batch: int):
-        # Method is used for visualizations
-        return self.__getitem__(idx_batch, apply_transforms=False)
+        return features, responses_with_weights
 
     def _get_features_responses_weights(self, index: int) -> Tuple[np.array, np.array, np.array]:
         raise NotImplementedError
@@ -93,7 +85,6 @@ class BaseSequence(keras.utils.Sequence):
 
 
 class MemmappedSequence(BaseSequence):
-    batches_per_epoch = None
 
     def __init__(
             self,
@@ -103,17 +94,13 @@ class MemmappedSequence(BaseSequence):
             feature_scaler: BaseGlobalScaler,
             response_scaler: BaseGlobalScaler,
             batch_size: int,
-            apply_transforms: bool,
+            apply_random_transforms: bool,
     ) -> None:
         self.features = features  # a list of numpy arrays, each of which is (n,y,x,f)
         self.responses = responses  # a list of numpy arrays, each of which is (n,y,x,r)
         self.weights = weights  # a list of numpy arrays, each of which is (n,y,x,1)
-        self.feature_scaler = feature_scaler
-        self.response_scaler = response_scaler
-        self.batch_size = batch_size
-        self.apply_transforms = apply_transforms
-
-        # TODO: Phil:  what does this do exactly?
+        super().__init__(feature_scaler=feature_scaler, response_scaler=response_scaler, batch_size=batch_size,
+                         apply_random_transforms=apply_random_transforms)
 
         # Determine the cumulative number of total samples across arrays - we're going to use
         # it to roll between files when exctracting samples
@@ -126,8 +113,6 @@ class MemmappedSequence(BaseSequence):
         return int(np.ceil(self.cum_samples_per_array[-1] / self.batch_size))
 
     def _get_features_responses_weights(self, index: int) -> Tuple[np.array, np.array, np.array]:
-        # TODO:  Phil:  could you comment so it's easier to see what's going on here?
-
         # start by finding which array we're starting in, based on the input index, batch size,
         # and the number of samples per array
         current_array = 0
