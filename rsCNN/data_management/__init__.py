a few bug fixes, added categorical weighting option into training_data, and built in parallel gpus

rsCNN/data_management/__init__.py
@@ -111,8 +111,8 @@ class DataConfig:
         self.feature_shape = None
 
         # Scalers
-        self.feature_scaler_name = kwargs.get('feature_scaler_name', None)
-        self.response_scaler_name = kwargs.get('response_scaler_name', None)
+        self.feature_scaler_name = kwargs.get('feature_scaler_name', 'NullScaler')
+        self.response_scaler_name = kwargs.get('response_scaler_name', 'NullScaler')
 
     # TODO: safegaurd from overwrite?
     def save_to_file(self):
@@ -145,7 +145,7 @@ def load_training_data(config: DataConfig):
             features.append(np.load(config.feature_files[fold], mmap_mode='r'))
         else:
             success = False
-            print('feailed read at ' + config.feature_files[fold])
+            print('failed read at ' + config.feature_files[fold])
             break
         if (os.path.isfile(config.response_files[fold])):
             responses.append(np.load(config.response_files[fold], mmap_mode='r'))

rsCNN/evaluation/networks.py
@@ -134,7 +134,7 @@ def visualize_feature_progression(data_sequence, model, compact=False, max_filte
             # Plot!
             ax = fig.add_axes([ip[0], ip[1], image_size, image_size], zorder=max_filters+1-_iii)
             top = max(top, ip[1]+image_size)
-            plt.imshow(tp, vmin=np.nanpercentile(tp, 5), vmax=np.nanpercentile(tp, 95))
+            plt.imshow(tp, vmin=np.nanpercentile(tp, 0), vmax=np.nanpercentile(tp, 100))
             adjust_axis(ax)
             if (_iii == 0):
                 plt.xlabel(layer_names[_l])

rsCNN/evaluation/results.py
@@ -16,6 +16,7 @@ def plot_raw_and_scaled_result_examples(data_sequence: BaseSequence, experiment:
     features = features[0]
     responses = responses[0]
     responses, weights = responses[..., :-1], responses[..., -1]
+    weights = weights.reshape((weights.shape[0],weights.shape[1],weights.shape[2],1))
 
     features[features == data_sequence.feature_scaler.nodata_value] = np.nan
     responses[responses == data_sequence.response_scaler.nodata_value] = np.nan
@@ -42,6 +43,7 @@ def plot_raw_and_scaled_result_examples(data_sequence: BaseSequence, experiment:
     trans_resp_mins, trans_resp_maxs = _get_mins_maxs(responses)
     pred_resp_mins, pred_resp_maxs = _get_mins_maxs(predictions)
     invtrans_pred_resp_mins, invtrans_pred_resp_maxs = _get_mins_maxs(raw_responses)
+    weight_mins, weight_maxs = _get_mins_maxs(weights)
 
     while _sample_ind < raw_responses.shape[0]:
         l_num_samp = min(max_samples_per_page, raw_responses.shape[0]-_sample_ind)
@@ -113,12 +115,13 @@ def plot_raw_and_scaled_result_examples(data_sequence: BaseSequence, experiment:
                         ax.add_patch(rect)
 
                 ax = plt.subplot(gs1[_s-_sample_ind, -1])
-                ax.imshow(np.squeeze(weights[_s, :, :]), vmin=0, vmax=1, cmap='Greys_r')
+                ax.imshow(np.squeeze(weights[_s, :, :]), vmin=weight_mins[0], vmax=weight_maxs[0], cmap='Greys_r')
                 plt.xticks([])
                 plt.yticks([])
 
                 if (_s == _sample_ind):
-                    plt.title('Weights')
+                    plt.title('Weights = \n' + 
+                              str(round(weight_mins[0], 2)) + '\n' + str(round(weight_maxs[0], 2)))
 
             plt.suptitle('Prediction Plots Page ' + str((len(fig_list))))
             fig_list.append(fig)