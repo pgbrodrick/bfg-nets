1c5a98d641d2cad7e7bb04c6b07d7c24beb81bdc
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-04-08 18:10:59 -0500

    Structures network module in a sensible way.

---
+++ rsCNN/networks/__init__.py
@@ -0,0 +1,1 @@
+# TODO:  structure imports so that relevant items are imported here, e.g., Experiment but not the crap that goes with it
---
+++ rsCNN/networks/histories.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+from typing import Union
+
+
+FILENAME_HISTORY = 'history.pkl'
+
+
+def load_history(dir_history: str) -> Union[dict, None]:
+    filepath = os.path.join(dir_history, FILENAME_HISTORY)
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'rb') as file_:
+        history = pickle.load(file_)
+    return history
+
+
+def save_history(history: dict, dir_history: str) -> None:
+    if not os.path.exists(dir_history):
+        os.makedirs(dir_history)
+    filepath = os.path.join(dir_history, FILENAME_HISTORY)
+    with open(filepath, 'wb') as file_:
+        pickle.dump(history, file_)
+
+
+def combine_histories(existing_history, new_history):
+    combined_history = existing_history.copy()
+    for key, value in new_history.items():
+        combined_history.setdefault(key, list()).extend(value)
+    return combined_history
