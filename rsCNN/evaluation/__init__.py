cf7bb6582546c2b22d75567060e328e59c8552b9
pbrodrick <pbrodrick@carnegiescience.edu>
2019-04-10 17:17:13 -0700

    correcting previous commit...dissabling CheckpointHistory fails, turning off parallel gpu for now till resolved

--- rsCNN/evaluation/__init__.py
+++ rsCNN/evaluation/__init__.py
@@ -24,6 +24,8 @@ class ExperimentReport(object):
     # realize this was the intent till I was all the way through, and want to make this change when I'm more fresh.
     def __init__(self, experiment: Experiment, train_sequence: BaseSequence, validation_sequence: BaseSequence = None, test_sequence: BaseSequence = None):
         self.experiment = experiment
+        self.train_sequence = train_sequence
+        self.validation_sequence = validation_sequence
         self.test_sequence = test_sequence
 
     def create_report(self):
@@ -34,33 +36,34 @@ class ExperimentReport(object):
             # Model history
             #pdf.savefig(networks.plot_history(self.experiment.history), bbox_inches='tight')
             # Input examples and their scaled representations
-            #for fig in rs_data.plot_raw_and_scaled_input_examples(self.test_sequence):
-            #    pdf.savefig(fig, bbox_inches='tight')
-            ## Output examples and their scaled representations
-            #for fig in results.plot_raw_and_scaled_result_examples(self.test_sequence, self.experiment):
-            #    pdf.savefig(fig, bbox_inches='tight')
-            ## Compact network visualization
-            #for fig in networks.visualize_feature_progression(self.test_sequence, self.experiment.model, compact=True):
-            #    pdf.savefig(fig, bbox_inches='tight')
-            ## Expanded network visualization
-            #for fig in networks.visualize_feature_progression(self.test_sequence, self.experiment.model):
-            #    pdf.savefig(fig, bbox_inches='tight')
+            for fig in rs_data.plot_raw_and_scaled_input_examples(self.train_sequence):
+                pdf.savefig(fig, bbox_inches='tight')
+            # Output examples and their scaled representations
+            for fig in results.plot_raw_and_scaled_result_examples(self.train_sequence, self.experiment):
+                pdf.savefig(fig, bbox_inches='tight')
+            # Compact network visualization
+            for fig in networks.visualize_feature_progression(self.train_sequence, self.experiment.model, compact=True):
+                pdf.savefig(fig, bbox_inches='tight')
+            # Expanded network visualization
+            for fig in networks.visualize_feature_progression(self.train_sequence, self.experiment.model):
+                pdf.savefig(fig, bbox_inches='tight')
 
-            ## TODO: Fabina, going through it more and I'd prefer not to have a separate ResultsReport class.  The only inherrent
-            ## benefit is not re-performing predictions.  That function is currently cheap, and even if it weren't we could just
-            ## do it here and pass it in, eliminating the need for a single-use class with one asset
+            # TODO: Fabina, going through it more and I'd prefer not to have a separate ResultsReport class.  The only inherrent
+            # benefit is not re-performing predictions.  That function is currently cheap, and even if it weren't we could just
+            # do it here and pass it in, eliminating the need for a single-use class with one asset
 
-            ## Plot Spatial Error
-            #for fig in results.spatial_error(self.test_sequence, self.experiment):
-            #    pdf.savefig(fig, bbox_inches='tight')
+            # Plot Spatial Error
+            for fig in results.spatial_error(self.train_sequence, self.experiment):
+                pdf.savefig(fig, bbox_inches='tight')
 
-            ## TODO: Fabina, I'm not happy with the way these sequences are called at all (see note above).
-            ## Plot Training Sequence
-            #for fig in results.single_sequence_prediction_histogram(self.test_sequence, self.experiment, 'Training'):
-            #    pdf.savefig(fig, bbox_inches='tight')
-            ## Plot Validation Sequence
-            #for fig in results.single_sequence_prediction_histogram(self.test_sequence, self.experiment, 'Validation'):
-            #    pdf.savefig(fig, bbox_inches='tight')
+            # TODO: Fabina, I'm not happy with the way these sequences are called at all (see note above).
+            # Plot Training Sequence
+            for fig in results.single_sequence_prediction_histogram(self.train_sequence, self.experiment, 'Training'):
+                pdf.savefig(fig, bbox_inches='tight')
+            # Plot Validation Sequence
+            if (self.validation_sequence is not None):
+                for fig in results.single_sequence_prediction_histogram(self.validation_sequence, self.experiment, 'Validation'):
+                    pdf.savefig(fig, bbox_inches='tight')
 
             # TODO
             # weight_visualization
