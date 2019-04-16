941983f979c4e5bc4a5cb60dec01eea25cc21328
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-04-16 18:05:13 -0500

    Calculates data range in Samples instead of min/max everywhere else.

--- rsCNN/evaluation/samples.py
+++ rsCNN/evaluation/samples.py
@@ -7,14 +7,22 @@ from rsCNN.data_management import scalers, sequences
 
 
 class Samples(object):
+    num_samples = None
     data_sequence = None
     raw_features = None
+    raw_features_range = None
     trans_features = None
+    trans_features_range = None
     raw_responses = None
+    raw_responses_range = None
     trans_responses = None
+    trans_responses_range = None
     raw_predictions = None
+    raw_predictions_range = None
     trans_predictions = None
+    trans_predictions_range = None
     weights = None
+    weights_range = None
 
     def __init__(self, data_sequence: sequences.BaseSequence, model: keras.Model) -> None:
         self.data_sequence = data_sequence
@@ -26,6 +34,7 @@ class Samples(object):
         # TODO:  handle getting representative samples
         # TODO:  handle multiple inputs
         features, responses = self.data_sequence.__getitem__(0)
+        self.num_samples = features[0].shape[0]
         # We expect weights to be the last element in the responses array
         self.weights = responses[0][..., -1]
         # TODO:  confirm this was not necessary:
@@ -59,3 +68,17 @@ class Samples(object):
             self.trans_responses = sampled_responses
             self.raw_predictions = self.data_sequence.response_scaler.inverse_transform(predictions)
             self.trans_predictions = predictions
+
+    def _set_raw_and_transformed_ranges(self):
+        self.raw_features_range = self._get_range(self.raw_features)
+        self.trans_features_range = self._get_range(self.trans_features)
+        self.raw_responses_range = self._get_range(self.raw_responses)
+        self.trans_responses_range = self._get_range(self.trans_responses)
+        self.raw_predictions_range = self._get_range(self.raw_predictions)
+        self.trans_predictions_range = self._get_range(self.trans_predictions)
+        self.weights_range = self._get_range(self.weights)
+
+    def _get_range(self, data: np.array) -> np.array:
+        mins = np.nanpercentile(data.reshape((-1, data.shape[-1])), 0, axis=0)
+        maxs = np.nanpercentile(data.reshape((-1, data.shape[-1])), 100, axis=0)
+        return np.dstack([mins, maxs])[0]
