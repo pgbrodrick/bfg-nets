47c2cbefe6d30351c9699883dc93c1a8309f2bd1
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-04-17 10:33:29 -0500

    Generalizes plots to predictions, incorporates in results.

--- rsCNN/evaluation/subplots.py
+++ rsCNN/evaluation/subplots.py
@@ -1,5 +1,6 @@
-from typing import Union
+from typing import Tuple, Union
 
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
 from rsCNN.evaluation import samples
@@ -17,7 +18,7 @@ def plot_raw_features(
         add_xlabel: bool,
         add_ylabel: bool
 ) -> None:
-    return _plot_sample_attribute(sampled, idx_sample, idx_feature, 'raw_features', ax, add_xlabel, add_ylabel)
+    _plot_sample_attribute(sampled, idx_sample, idx_feature, 'raw_features', ax, add_xlabel, add_ylabel)
 
 
 def plot_transformed_features(
@@ -28,35 +29,59 @@ def plot_transformed_features(
         add_xlabel: bool,
         add_ylabel: bool
 ) -> None:
-    return _plot_sample_attribute(sampled, idx_sample, idx_feature, 'trans_features', ax, add_xlabel, add_ylabel)
+    _plot_sample_attribute(sampled, idx_sample, idx_feature, 'trans_features', ax, add_xlabel, add_ylabel)
 
 
 def plot_raw_responses(
         sampled: samples.Samples,
         idx_sample: int,
-        idx_feature: int,
+        idx_response: int,
         ax: plt.Axes,
         add_xlabel: bool,
         add_ylabel: bool
 ) -> None:
-    return _plot_sample_attribute(sampled, idx_sample, idx_feature, 'raw_responses', ax, add_xlabel, add_ylabel)
+    _plot_sample_attribute(sampled, idx_sample, idx_response, 'raw_responses', ax, add_xlabel, add_ylabel)
 
 
 def plot_transformed_responses(
         sampled: samples.Samples,
         idx_sample: int,
-        idx_feature: int,
+        idx_response: int,
         ax: plt.Axes,
         add_xlabel: bool,
         add_ylabel: bool
 ) -> None:
-    return _plot_sample_attribute(sampled, idx_sample, idx_feature, 'trans_responses', ax, add_xlabel, add_ylabel)
+    _plot_sample_attribute(sampled, idx_sample, idx_response, 'trans_responses', ax, add_xlabel, add_ylabel)
+
+
+def plot_raw_predictions(
+        sampled: samples.Samples,
+        idx_sample: int,
+        idx_response: int,
+        ax: plt.Axes,
+        add_xlabel: bool,
+        add_ylabel: bool
+) -> None:
+    _plot_sample_attribute(sampled, idx_sample, idx_response, 'raw_predictions', ax, add_xlabel, add_ylabel)
+    _add_internal_window_to_subplot(sampled, ax)
+
+
+def plot_transformed_predictions(
+        sampled: samples.Samples,
+        idx_sample: int,
+        idx_response: int,
+        ax: plt.Axes,
+        add_xlabel: bool,
+        add_ylabel: bool
+) -> None:
+    _plot_sample_attribute(sampled, idx_sample, idx_response, 'trans_predictions', ax, add_xlabel, add_ylabel)
+    _add_internal_window_to_subplot(sampled, ax)
 
 
 def _plot_sample_attribute(
         sampled: samples.Samples,
         idx_sample: int,
-        idx_feature: int,
+        idx_axis: int,
         attribute_name: str,
         ax: plt.Axes,
         add_xlabel: bool,
@@ -64,9 +89,9 @@ def _plot_sample_attribute(
 ) -> None:
     attribute = getattr(sampled, attribute_name)
     range_ = getattr(sampled, attribute_name + '_range')
-    y_label = '\n'.join(word.capitalize() for word in attribute_name.split('_')).rstrip('s')
-    min_, max_ = range_[idx_feature, :]
-    ax.imshow(attribute[idx_sample, :, :, idx_feature], vmin=min_, vmax=max_)
+    y_label = '\n'.join(word.capitalize() for word in attribute_name.split('_') if word != 'raw').rstrip('s')
+    min_, max_ = range_[idx_axis, :]
+    ax.imshow(attribute[idx_sample, :, :, idx_axis], vmin=min_, vmax=max_)
     ax.set_xticks([])
     ax.set_yticks([])
     if add_xlabel:
@@ -82,6 +107,19 @@ def plot_weights(sampled: samples.Samples, ax: plt.Axes, add_xlabel: bool) -> No
     ax.set_yticks([])
     if add_xlabel:
         ax.set_xlabel('Weights\n{}\n{}'.format(_format_number(min_), _format_number(max_)))
+
+
+def _add_internal_window_to_subplot(sampled: samples.Samples, ax: plt.Axes) -> None:
+    inshape = sampled.network_config['architecture']['inshape'],
+    internal_window_radius = sampled.network_config['architecture']['internal_window_radius'],
+    if (internal_window_radius*2 == inshape[0]):
+        return
+    buffer = (inshape[0] - internal_window_radius * 2) / 2
+    rect = patches.Rectangle(
+        (buffer, buffer), internal_window_radius * 2, internal_window_radius * 2, linewidth=1, edgecolor='white',
+        facecolor='none'
+    )
+    ax.add_patch(rect)
 
 
 def _format_number(number: Union[int, float]) -> str:
