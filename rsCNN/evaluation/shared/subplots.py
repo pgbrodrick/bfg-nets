Fixes idx_sample needed for softmax and weights, xlabel/ylabel order.

rsCNN/evaluation/shared/subplots.py
@@ -105,31 +105,35 @@ def _plot_sample_attribute(
     ax.set_xticks([])
     ax.set_yticks([])
     if add_xlabel:
-        ax.set_xlabel('Sample\n{}'.format(idx_sample))
+        ax.set_xlabel('{}\n{}\n{}'.format(y_label, _format_number(min_), _format_number(max_)))
     if add_ylabel:
-        ax.set_ylabel('{}\n{}\n{}'.format(y_label, _format_number(min_), _format_number(max_)))
+        ax.set_ylabel('Sample\n{}'.format(idx_sample))
 
 
-def plot_weights(sampled: samples.Samples, ax: plt.Axes, add_xlabel: bool) -> None:
+def plot_softmax(sampled: samples.Samples, idx_sample: int, ax: plt.Axes, add_xlabel: bool, add_ylabel: bool) -> None:
+    # Note:  this assumes that the softmax applied to all prediction axes
+    min_, max_ = (0, sampled.raw_predictions.shape[-1] - 1)
+    ax.imshow(np.argmax(sampled.raw_predictions[idx_sample, :], axis=-1), vmin=min_, vmax=max_)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    if add_xlabel:
+        # TODO:  Phil:  better label?
+        ax.set_xlabel('Softmax\nCategories\n{}\n{}'.format(0, _format_number(max_)))
+    if add_ylabel:
+        ax.set_ylabel('Sample\n{}'.format(idx_sample))
+
+
+def plot_weights(sampled: samples.Samples, idx_sample: int, ax: plt.Axes, add_xlabel: bool, add_ylabel: bool) -> None:
     min_, max_ = sampled.weights_range[0, :]
-    weights = sampled.weights.copy()
+    weights = sampled.weights[idx_sample, :].copy()
     weights[weights == 0] = np.nan
     ax.imshow(weights, vmin=min_, vmax=max_, cmap=_COLORMAP_WEIGHTS)
     ax.set_xticks([])
     ax.set_yticks([])
     if add_xlabel:
         ax.set_xlabel('Weights\n{}\n{}'.format(_format_number(min_), _format_number(max_)))
-
-
-def plot_softmax(sampled: samples.Samples, ax: plt.Axes, add_xlabel: bool) -> None:
-    # Note:  this assumes that the softmax applied to all prediction axes
-    min_, max_ = (0, sampled.raw_predictions.shape[-1] - 1)
-    ax.imshow(np.argmax(sampled.raw_predictions, axis=-1), vmin=min_, vmax=max_)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    if add_xlabel:
-        # TODO:  Phil:  better label?
-        ax.set_xlabel('Softmax\nCategories\n{}\n{}'.format(0, _format_number(max_)))
+    if add_ylabel:
+        ax.set_ylabel('Sample\n{}'.format(idx_sample))
 
 
 def _add_internal_window_to_subplot(sampled: samples.Samples, ax: plt.Axes) -> None: