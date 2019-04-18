1a5e77482f5a9d53674e60af14af10a7f31ab9b0
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-04-18 08:44:18 -0500

    Handles idx_col and axis iteration in generator for clarity.

--- rsCNN/evaluation/shared/__init__.py
+++ rsCNN/evaluation/shared/__init__.py
@@ -1,5 +1,6 @@
 from rsCNN.evaluation.shared.figures \
-    import get_figure_and_grid, plot_figures_iterating_through_samples_features_responses
+    import get_figure_and_grid, get_axis_generator_for_sample_row, \
+    plot_figures_iterating_through_samples_features_responses
 from rsCNN.evaluation.shared.subplots \
     import plot_raw_features, plot_transformed_features, plot_raw_responses, plot_transformed_responses, \
     plot_raw_predictions, plot_transformed_predictions, plot_softmax, plot_weights, plot_error_categorical, \
