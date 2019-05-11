0e33e73882791e48178d7d090197cd4c4c64471b
nsfabina <nicholas.fabina@gmail.com>
2019-05-10 17:18:45 -0700

    Fixes various minor issues.
    Fixes bug variable not assigned yet.
    
    Fixes bug.
    
    Fixes.
    
    Fix

--- rsCNN/networks/architectures/shared.py
+++ rsCNN/networks/architectures/shared.py
@@ -3,7 +3,6 @@ from rsCNN.configs import BaseConfigSection, ConfigOption
 
 DEFAULT_FILTERS = 64
 DEFAULT_KERNEL_SIZE = (3, 3)
-DEFAULT_OPTIMIZER = 'adam'
 DEFAULT_PADDING = 'same'
 DEFAULT_USE_BATCH_NORM = True
 DEFAULT_USE_INITIAL_COLORSPACE_TRANSFORMATION_LAYER = False
@@ -17,7 +16,6 @@ class BaseArchitectureOptions(BaseConfigSection):
     inshape = None
     kernel_size = None
     n_classes = None
-    optimizer = None
     output_activation = None
     padding = None
     use_batch_norm = None
@@ -27,12 +25,11 @@ class BaseArchitectureOptions(BaseConfigSection):
         ConfigOption('inshape', None, tuple),
         ConfigOption('kernel_size', DEFAULT_KERNEL_SIZE, tuple),
         ConfigOption('n_classes', None, int),
-        ConfigOption('optimizer', DEFAULT_OPTIMIZER, str),
         ConfigOption('output_activation', None, str),
         ConfigOption('padding', DEFAULT_PADDING, str),
         ConfigOption('use_batch_norm', DEFAULT_USE_BATCH_NORM, bool),
         ConfigOption(
-            'use_initial_colorspaced_transformation_layer', DEFAULT_USE_INITIAL_COLORSPACE_TRANSFORMATION_LAYER, bool),
+            'use_initial_colorspace_transformation_layer', DEFAULT_USE_INITIAL_COLORSPACE_TRANSFORMATION_LAYER, bool),
     ]
     _config_options_extra = list()
 
