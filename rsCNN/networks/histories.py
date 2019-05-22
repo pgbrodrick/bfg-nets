d7601c92d3318b77c2c04d4d62e45d86323d6783
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-05-22 11:31:15 -0500

    Adds logging for model/history save/load.

--- rsCNN/networks/histories.py
+++ rsCNN/networks/histories.py
@@ -2,6 +2,11 @@ import os
 import pickle
 from typing import Union
 
+from rsCNN.utils import logging
+
+
+_logger = logging.get_child_logger(__name__)
+
 
 FILENAME_HISTORY = 'history.pkl'
 
@@ -9,7 +14,9 @@ FILENAME_HISTORY = 'history.pkl'
 def load_history(dir_history: str, filename: str = None) -> Union[dict, None]:
     filepath = os.path.join(dir_history, filename or FILENAME_HISTORY)
     if not os.path.exists(filepath):
+        _logger.debug('History not loaded; file does not exist at {}'.format(filepath))
         return None
+    _logger.debug('Load history from {}'.format(filepath))
     with open(filepath, 'rb') as file_:
         history = pickle.load(file_)
     return history
@@ -17,8 +24,10 @@ def load_history(dir_history: str, filename: str = None) -> Union[dict, None]:
 
 def save_history(history: dict, dir_history: str, filename: str = None) -> None:
     if not os.path.exists(dir_history):
+        _logger.debug('Create directory to save history at {}'.format(dir_history))
         os.makedirs(dir_history)
     filepath = os.path.join(dir_history, filename or FILENAME_HISTORY)
+    _logger.debug('Save history to {}'.format(filepath))
     with open(filepath, 'wb') as file_:
         pickle.dump(history, file_)
 
