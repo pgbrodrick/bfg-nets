9df43b4642d2b3f236219cd5a20cb36bc4d1e1a8
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-05-27 17:03:04 -0500

    Cleans up experiment API and logic, adds documentation.

--- rsCNN/networks/experiments.py
+++ rsCNN/networks/experiments.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-import keras
 import keras.backend as K
 import numpy as np
 
@@ -15,32 +14,32 @@ from rsCNN.utils import gpus
 _logger = logging.getLogger(__name__)
 
 
-# TODO: rename Experiment to something more reasonable, as well as this file
 class Experiment(object):
     config = None
+    """configs.Config: rsCNN configuration object."""
     model = None
+    """keras.models.Model: Keras model object."""
     history = None
-    resume = None
+    """dict: Model training history."""
+    loaded_existing_history = None
+    """bool: Whether an existing history object was loaded from the model training directory."""
+    loaded_existing_model = None
+    """bool: Whether an existing model object was loaded from the model training directory."""
     train_sequence = None
     validation_sequence = None
     test_sequence = None
 
-    def __init__(self, config: configs.Config, resume: bool = False) -> None:
+    def __init__(self, config: configs.Config) -> None:
         self.config = config
-        self.resume = resume
-
-        if os.path.exists(self.config.model_training.dir_out):
-            if histories.load_history(get_history_filepath(self.config.model_training.dir_out)):
-                assert self.resume, 'Resume must be true to continue training an existing model'
-            # TODO:  do we need to assert that the configs are the same still?
-            #original_config = network_configs.load_network_config(self.network_config['model']['dir_out'])
-            # differing_items = network_configs.compare_network_configs_get_differing_items(
-            #    original_config, self.network_config)
-            # assert len(differing_items) == 0, \
-            #    'Provided network config differs from network config for existing, trained model:  {}'.format(
-            #        differing_items)
-        else:
+        if not os.path.exists(self.config.model_training.dir_out):
             os.makedirs(self.config.model_training.dir_out)
+        if os.path.exists(get_config_filepath(self.config.model_training.dir_out)):
+            config_existing = configs.create_config_from_file(get_config_filepath(self.config.model_training.dir_out))
+            config_differences = configs.get_config_differences(self.config, config_existing)
+            assert not config_differences, \
+                'Provided configuration differs from existing configuration at {}; differing values: {}'.format(
+                    get_config_filepath(self.config.model_training.dir_out), config_differences)
+        else:
             configs.save_config_to_file(self.config, get_config_filepath(self.config.model_training.dir_out))
 
     def build_or_load_model(self):
@@ -51,21 +50,38 @@ class Experiment(object):
             2 * self.config.data_build.loss_window_radius,
             self.config.model_training.weighted
         )
-        self.history = histories.load_history(get_history_filepath(self.config.model_training.dir_out))
-        if self.history:
-            _logger.debug('History exists in out directory, loading model from same location')
+        if os.path.exists(get_model_filepath(self.config.model_training.dir_out)):
+            _logger.debug('Loading existing model from model training directory at {}'.format(
+                get_model_filepath(self.config.model_training.dir_out)))
+            self.loaded_existing_model = True
             self.model = models.load_model(
                 self.config.model_training.dir_out, custom_objects={'_cropped_loss': loss_function})
-            if 'lr' in self.history:
-                _logger.debug('Setting learning rate to value from last training epoch')
-                K.set_value(self.model.optimizer.lr, self.history['lr'][-1])
-            # TODO:  do we want to warn or raise or nothing if the network type doesn't match the model type?
         else:
-            _logger.debug('History does not exist in model out directory, creating new history and model')
-            self.history = {'model_name': self.config.model_training.dir_out}
+            _logger.debug('Building new model, no model exists at {}'.format(
+                get_model_filepath(self.config.model_training.dir_out)))
+            self.loaded_existing_model = False
             self.model = config_sections.create_model_from_architecture_config_section(
                 self.config.model_training.architecture_name, self.config.architecture)
             self.model.compile(loss=loss_function, optimizer=self.config.model_training.optimizer)
+        if os.path.exists(get_history_filepath(self.config.model_training.dir_out)):
+            assert self.loaded_existing_model, \
+                'Model training history exists in model training directory, but existing model found; directory: {}' \
+                .format(self.config.model_training.dir_out)
+            _logger.debug('Loading existing history from model training directory at {}'.format(
+                get_history_filepath(self.config.model_training.dir_out)))
+            self.loaded_existing_history = True
+            self.history = histories.load_history(get_history_filepath(self.config.model_training.dir_out))
+            if 'lr' in self.history:
+                learning_rate = self.history['lr'][-1]
+                _logger.debug('Setting learning rate to value from last training epoch: {}'.format(learning_rate))
+                K.set_value(self.model.optimizer.lr, learning_rate)
+        else:
+            assert not self.loaded_existing_model, \
+                'Trained model exists in model training directory, but no training history found; directory: {}' \
+                .format(self.config.model_training.dir_out)
+            self.loaded_existing_history = False
+            self.history = {'model_name': self.config.model_training.dir_out}
+        """    
         # TODO:  reimplement multiple GPUs
         #n_gpu_avail = gpus.get_count_available_gpus()
         #_logger.debug('Using multiple GPUs with {} available'.format(n_gpu_avail))
@@ -75,38 +91,16 @@ class Experiment(object):
             self.model = keras.utils.multi_gpu_model(self._original_model, gpus=n_gpu_avail, cpu_relocation=True)
             self.model.callback_model = self._original_model
             self.model.compile(loss=loss_function, optimizer=self.config.model_training.optimizer)
-
-    def calculate_training_memory_usage(self, batch_size: int) -> float:
-        # Shamelessly copied from
-        # https://stackoverflow.com/questions/43137288/how-to-determine-needed-memory-of-keras-model
-        # but not tested rigorously
-        shapes_mem_count = 0
-        for l in self.model.layers:
-            single_layer_mem = 1
-            for s in l.output_shape:
-                if s is None:
-                    continue
-                single_layer_mem *= s
-            shapes_mem_count += single_layer_mem
-
-        trainable_count = np.sum([K.count_params(p) for p in set(self.model.trainable_weights)])
-        non_trainable_count = np.sum([K.count_params(p) for p in set(self.model.non_trainable_weights)])
-
-        number_size = 4.0
-        if K.floatx() == 'float16':
-            number_size = 2.0
-        if K.floatx() == 'float64':
-            number_size = 8.0
-
-        total_memory = number_size*(batch_size*shapes_mem_count + trainable_count + non_trainable_count)
-        gbytes = np.round(total_memory / (1024.0 ** 3), 3)
-        return gbytes
+        """
 
     def fit_network(
             self,
             train_sequence: BaseSequence,
-            validation_sequence: BaseSequence = None
+            validation_sequence: BaseSequence = None,
+            resume_training: bool = False
     ) -> None:
+        if self.history:
+            assert resume_training, 'Resume must be true to continue training an existing model'
 
         if self.config.model_training.assert_gpu:
             gpus.assert_gpu_available()
@@ -132,14 +126,70 @@ class Experiment(object):
         histories.save_history(self.history, get_history_filepath(self.config.model_training.dir_out))
         models.save_model(self.model, get_model_filepath(self.config.model_training.dir_out))
 
+    def calculate_model_memory_footprint(self, batch_size: int) -> float:
+        """Calculate model memory footprint. Shamelessly copied from (but not tested rigorously):
+        https://stackoverflow.com/questions/43137288/how-to-determine-needed-memory-of-keras-model.
+        Args:
+            batch_size:
+
+        Returns:
+            Model memory footprint in gigabytes.
+        """
+        shapes_mem_count = 0
+        for l in self.model.layers:
+            single_layer_mem = 1
+            for s in l.output_shape:
+                if s is None:
+                    continue
+                single_layer_mem *= s
+            shapes_mem_count += single_layer_mem
+
+        trainable_count = np.sum([K.count_params(p) for p in set(self.model.trainable_weights)])
+        non_trainable_count = np.sum([K.count_params(p) for p in set(self.model.non_trainable_weights)])
+
+        if K.floatx() == 'float16':
+            number_size = 2.0
+        elif K.floatx() == 'float32':
+            number_size = 4.0
+        if K.floatx() == 'float64':
+            number_size = 8.0
+
+        total_memory = number_size*(batch_size*shapes_mem_count + trainable_count + non_trainable_count)
+        gbytes = np.round(total_memory / (1024.0 ** 3), 3)
+        return gbytes
+
 
 def get_config_filepath(dir_out: str) -> str:
+    """Get the default config path for experiments.
+
+    Args:
+        dir_out: Experiment directory.
+
+    Returns:
+        Filepath to config.
+    """
     return os.path.join(dir_out, configs.DEFAULT_FILENAME_CONFIG)
 
 
 def get_history_filepath(dir_out: str) -> str:
+    """Get the default model training history path for experiments.
+
+    Args:
+        dir_out: Experiment directory.
+
+    Returns:
+        Filepath to model training history.
+    """
     return os.path.join(dir_out, histories.DEFAULT_FILENAME_HISTORY)
 
 
 def get_model_filepath(dir_out: str) -> str:
+    """Get the default model path for experiments.
+
+    Args:
+        dir_out: Experiment directory.
+
+    Returns:
+        Filepath to model.
+    """
     return os.path.join(dir_out, models.DEFAULT_FILENAME_MODEL)
