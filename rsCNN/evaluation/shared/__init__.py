ea6a2d29c4e4c27d644ec30126e7920cdc7bfcba
pbrodrick_pc <phil.brodrick@gmail.com>
2019-05-30 12:54:51 -0700

    handled a series of todos and general fixes in error report generation

--- rsCNN/evaluation/shared/__init__.py
+++ rsCNN/evaluation/shared/__init__.py
@@ -4,5 +4,5 @@ from rsCNN.evaluation.shared.figures \
     plot_figures_iterating_through_samples_features_responses
 from rsCNN.evaluation.shared.subplots \
     import plot_raw_features, plot_transformed_features, plot_raw_responses, plot_transformed_responses, \
-    plot_raw_predictions, plot_transformed_predictions, plot_softmax, plot_weights, plot_error_categorical, \
+    plot_raw_predictions, plot_transformed_predictions, plot_max_likelihood, plot_weights, plot_binary_error, \
     plot_raw_error_regression, plot_transformed_error_regression, add_internal_window_to_subplot
