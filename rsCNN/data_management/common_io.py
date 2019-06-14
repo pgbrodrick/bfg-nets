11e192184df5169f367bbfbffc111fd7e45eb910
pbrodrick_pc <phil.brodrick@gmail.com>
2019-06-14 08:49:08 -0700

    fixing repo shenanigans

--- rsCNN/data_management/common_io.py
+++ rsCNN/data_management/common_io.py
@@ -58,11 +58,6 @@ def get_overlapping_extent(dataset_list_of_lists: List[List[gdal.Dataset]]):
             local_list.append(None)
 
         local_list = np.array(local_list).astype(int)
-
-        # special case of no boundary file.  Append a None if no value:
-        if (len(local_list) == 0):
-            local_list.append(None)
-
         return_ul_list.append(local_list)
     return return_ul_list, x_len, y_len
 
