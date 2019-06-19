57606350fe69119e6f23bc9a7dc54bd7663024d5
Phil Brodrick <pbrodrick@carnegiescience.edu>
2019-06-18 17:26:45 -0700

    overhaul of data input format to be Data_Container oriented.  Includes reshuffling of config checks and error messages, and example rebuild

--- examples/flight_line_extrapolation_test.py
+++ examples/flight_line_extrapolation_test.py
@@ -1,72 +1,53 @@
 import argparse
 import os
-import sys
-import yaml
 
-# TODO manage imports
-from rsCNN.networks.experiments import Experiment
-import rsCNN.data_management
-from rsCNN.networks.network_configs import create_network_config
-import rsCNN.data_management.apply_model_to_data
+from rsCNN.configuration import configs
+from rsCNN.data_management import data_core
 import rsCNN.evaluation
-from rsCNN.data_management import training_data, sequences
-
+from rsCNN.experiments import experiments
 from rsCNN.utils import logging
-
-logger = logging.get_root_logger('debug_out.out')
-_LOG_LEVEL = 'DEBUG'  # 'TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR'
-
-logger.setLevel(_LOG_LEVEL)
-
 
 parser = argparse.ArgumentParser(description='Example spatial extrapolation application')
 parser.add_argument('settings_file')
-parser.add_argument('key')
+parser.add_argument('key', type=str, choices=['all', 'train', 'report', 'apply'])
+parser.add_argument('-debug_level', type=str, default='INFO', choices=['DEBUG', 'INFO', 'WARN', 'ERROR'])
+parser.add_argument('-debug_file', type=str, default='debug.out')
 args = parser.parse_args()
 
+logger = logging.get_root_logger(args.debug_file)
+logger.setLevel(args.debug_level)
+
 assert os.path.isfile(args.settings_file), 'Settings file: ' + args.settings_file + ' does not exist'
-global_options = yaml.load(open(args.settings_file, 'r'))
+config = configs.create_config_from_file(args.settings_file)
 
-global_options['raw_feature_file_list'] = [['../global_cwc/dat/features/feat_subset.tif']]
-global_options['raw_response_file_list'] = [['../global_cwc/dat/responses/resp_subset.tif']]
+config['raw_feature_file_list'] = [['../global_cwc/dat/features/feat_subset.tif']]
+config['raw_response_file_list'] = [['../global_cwc/dat/responses/resp_subset.tif']]
 
+data_container = data_core.Data_Container(config)
 
-data_config = rsCNN.data_management.DataConfig(**global_options)
-
-data_container = training_data.build_or_load_rawfile_data(data_config)
+data_container.build_or_load_rawfile_data()
 data_container.build_or_load_scalers()
+data_container.load_sequences()
 
-training_sequence = sequences.build_memmapped_sequence(data_container, data_container.train_folds, batch_size=100)
-validation_sequence = sequences.build_memmapped_sequence(
-    data_container, [data_container.config.validation_fold], batch_size=100)
-
-# Move the inshape intot he build_or_load_model
-network_config = create_network_config(
-    inshape=(data_config.window_radius*2, data_config.window_radius*2, 3), **global_options)
-
-experiment = Experiment(network_config, resume=True)
+experiment = experiments.Experiment(config)
 experiment.build_or_load_model()
 
+if (args.key == 'prelim_report'):
+    rsCNN.evaluation.create_preliminary_model_report_from_experiment(experiment, data_container)
+
 if (args.key == 'train' or args.key == 'all'):
-    experiment.fit_network(training_sequence, validation_sequence)
+    experiment.fit_model_with_dataset(data_container, resume_training=True)
 
-if (args.key == 'report'):
-    rsCNN.evaluation.create_model_report(experiment.model,
-                                         experiment.network_config,
-                                         training_sequence,
-                                         validation_sequence,
-                                         history=experiment.history)
-
+if (args.key == 'report' or args.key == 'all'):
+    rsCNN.evaluation.create_model_report_from_experiment(experiment, data_container)
 
 if (args.key == 'apply' or args.key == 'all'):
     application_feature_files = ['../global_cwc/dat/features/feat_subset.tif']
     application_output_basenames = ['examples/output/feat_subset_applied_cnn.tif']
     for _f in range(len(application_feature_files)):
         rsCNN.data_management.apply_model_to_data.apply_model_to_raster(experiment.model,
-                                                                        data_config,
+                                                                        data_container,
                                                                         application_feature_files[_f],
                                                                         application_output_basenames[_f],
                                                                         make_png=False,
-                                                                        make_tif=True,
-                                                                        feature_transformer=data_container.feature_scaler,
-                                                                        response_transformer=data_container.response_scaler)
+                                                                        make_tif=True)
