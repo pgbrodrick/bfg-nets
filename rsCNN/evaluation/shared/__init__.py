7c01b30271874d7a6f9a37087857e2fd5ccf884b
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-06-20 15:06:21 -0500

    Refactors/reorganizes evaluation module, updates naming for clarity.

--- rsCNN/evaluation/shared/__init__.py
+++
@@ -1,8 +0,0 @@
-from rsCNN.evaluation.shared.colormaps import COLORMAP_CATEGORICAL, COLORMAP_ERROR, COLORMAP_METRICS, COLORMAP_WEIGHTS
-from rsCNN.evaluation.shared.figures \
-    import get_figure_and_grid, get_axis_iterator_for_sample_row, \
-    plot_figures_iterating_through_samples_features_responses
-from rsCNN.evaluation.shared.subplots \
-    import plot_raw_features, plot_transformed_features, plot_raw_responses, plot_transformed_responses, \
-    plot_raw_predictions, plot_transformed_predictions, plot_max_likelihood, plot_weights, plot_binary_error, \
-    plot_raw_error_regression, plot_transformed_error_regression, add_internal_window_to_subplot
