8756b195ec27f808a477c1092a9ed83a10e592c2
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-06-20 17:08:41 -0500

    Updates checking obsolete options, fixes issues that popped up.

--- rsCNN/configuration/check_config_parameters_not_obsolete.py
+++ rsCNN/configuration/check_config_parameters_not_obsolete.py
@@ -1,7 +1,7 @@
 import os
 import shlex
 import subprocess
-from typing import List
+from typing import List, Tuple
 
 from rsCNN import architectures, utils
 from rsCNN.configuration import configs, sections
@@ -16,41 +16,48 @@ def check_config_parameters_not_obsolete():
     _logger.info('Check config parameters are not obsolete')
     _logger.warning('NOTE: this check may have false positives or negatives depending on how parameters are referenced')
     all_obsolete = list()
+    num_all_obsolete = 0
     architecture_names = architectures.get_available_architectures()
     for idx_config, architecture_name in enumerate(architecture_names):
         config = configs.create_config_template(architecture_name)
         if idx_config == 0:
-            all_obsolete.extend(_check_generic_config_parameters(config))
-        all_obsolete.append(_check_architecture_config_parameters(config))
-    _logger.info('Config parameter check complete')
+            all_generic_obsolete, num_obsolete = _check_generic_config_parameters(config)
+            all_obsolete.extend(all_generic_obsolete)
+            num_all_obsolete += num_obsolete
+        architecture_obsolete, num_architecture_obsolete = _check_architecture_config_parameters(config)
+        all_obsolete.append(architecture_obsolete)
+        num_all_obsolete += num_architecture_obsolete
+    _logger.info('Config parameter check complete:  found {} potentially obsolete parameters'.format(num_all_obsolete))
     return all_obsolete
 
 
-def _check_generic_config_parameters(config: configs.Config) -> List[str]:
+def _check_generic_config_parameters(config: configs.Config) -> Tuple[List[str], int]:
     all_obsolete = list()
+    num_obsolete = 0
     for config_section in sections.get_config_sections():
         section_name = config_section.get_config_name_as_snake_case()
         _logger.debug('Check options for config section {}'.format(section_name))
         section = getattr(config, section_name)
         obsolete = [option_key for option_key in section.get_option_keys()
                     if not _is_option_key_in_package(option_key, section_name)]
-        message = 'Config section {} has {} obsolete parameters'.format(section_name, len(obsolete))
+        message = 'Config section {} has {} potentially obsolete parameters'.format(section_name, len(obsolete))
         if obsolete:
             message += ':  {}'.format(', '.join(obsolete))
         _logger.info(message)
         all_obsolete.append(message)
-    return all_obsolete
+        num_obsolete += len(obsolete)
+    return all_obsolete, num_obsolete
 
 
-def _check_architecture_config_parameters(config: configs.Config) -> str:
+def _check_architecture_config_parameters(config: configs.Config) -> Tuple[str, int]:
     obsolete = [option_key for option_key in config.architecture.get_option_keys()
                 if _is_option_key_in_architecture_module(option_key, config.model_training.architecture_name)]
-    message = 'Architecture config section for architecture {} has {} obsolete parameters'.format(
+    message = 'Architecture config section for architecture {} has {} potentially obsolete parameters'.format(
         config.model_training.architecture_name, len(obsolete))
     if obsolete:
         message += ':  {}'.format(', '.join(obsolete))
     _logger.info(message)
-    return message
+    return message, len(obsolete)
 
 
 def _is_option_key_in_package(option_key: str, config_section: str) -> bool:
