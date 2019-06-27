f3076b8c396bcf52f72df0c5190f4b160bea4e8c
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-06-27 17:28:57 -0500

    Checks for continuous or categorical data types more specifically.

--- rsCNN/reporting/reports.py
+++ rsCNN/reporting/reports.py
@@ -18,6 +18,9 @@ _logger = logging.getLogger(__name__)
 
 _FILENAME_MODEL_REPORT = 'model_report.pdf'
 
+_LABEL_CATEGORICAL = 'CATEGORICAL'
+_LABEL_CONTINUOUS = 'CONTINUOUS'
+
 
 class Reporter(object):
     data_container = None
@@ -58,7 +61,7 @@ class Reporter(object):
                 self._add_figures(histories.plot_history(self.experiment.history), pdf)
 
     def _create_model_report_for_sequence(self, sampled: samples.Samples, pdf: PdfPages) -> None:
-        if self.experiment.is_model_trained and self.config.architecture.output_activation == 'softmax':
+        if self.experiment.is_model_trained and self._get_response_data_types() is _LABEL_CATEGORICAL:
             self._add_figures(self.plot_classification_report(sampled), pdf)
             self._add_figures(self.plot_confusion_matrix(sampled), pdf, tight=False)
         self._add_figures(self.plot_single_sequence_prediction_histogram(sampled), pdf)
@@ -110,9 +113,9 @@ class Reporter(object):
     ) -> List[plt.Figure]:
         assert self.experiment.is_model_trained, \
             'Cannot plot raw and transformed prediction samples because model is not trained.'
-        if self.config.architecture.output_activation == 'softmax':
+        if self._get_response_data_types() is _LABEL_CATEGORICAL:
             plotter = samples_viz.plot_classification_samples
-        else:
+        elif self._get_response_data_types() is _LABEL_CONTINUOUS:
             plotter = samples_viz.plot_regression_samples
         return plotter(
             sampled,
@@ -137,9 +140,9 @@ class Reporter(object):
             max_responses_per_page: int = None
     ) -> List[plt.Figure]:
         assert self.experiment.is_model_trained, 'Cannot plot spatial error because model is not trained.'
-        if self.config.architecture.output_activation == 'softmax':
+        if self._get_response_data_types() is _LABEL_CATEGORICAL:
             plotter = model_performance.plot_spatial_classification_error
-        else:
+        elif self._get_response_data_types() is _LABEL_CONTINUOUS:
             plotter = model_performance.plot_spatial_regression_error
         return plotter(
             sampled,
@@ -150,3 +153,14 @@ class Reporter(object):
     def plot_classification_report(self, sampled: samples.Samples) -> List[plt.Figure]:
         assert self.experiment.is_model_trained, 'Cannot plot classification report because model is not trained.'
         return model_performance.plot_classification_report(sampled)
+
+    def _get_response_data_types(self) -> str:
+        data_types = set([data_type for file_dt in self.config.raw_files.response_data_type for data_type in file_dt])
+        if data_types == {'C'}:
+            return _LABEL_CATEGORICAL
+        elif data_types == {'R'}:
+            return _LABEL_CONTINUOUS
+        elif data_types == {'C', 'R'}:
+            raise AssertionError('Reporter does not currently support mixed response data types.')
+        else:
+            raise AssertionError('Unexpected data types found: {}.'.format(data_types))
