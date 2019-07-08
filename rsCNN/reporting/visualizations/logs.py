5b26285cb722eb519906e18cb9fe6d491e4b0471
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-08 15:56:48 -0500

    Fixes text wrapping in log plots.

--- rsCNN/reporting/visualizations/logs.py
+++ rsCNN/reporting/visualizations/logs.py
@@ -1,5 +1,6 @@
 import os
 import re
+import textwrap
 from typing import List
 
 import matplotlib.pyplot as plt
@@ -10,6 +11,10 @@ from rsCNN.experiments import experiments
 
 
 plt.switch_backend('Agg')  # Needed for remote server plotting
+
+
+_LINE_CHARACTER_LIMIT = 140
+_LINE_INDENT = '    '
 
 
 def plot_log_warnings_and_errors(config_data: configs.Config, config_model: configs.Config) -> List[plt.Figure]:
@@ -24,16 +29,21 @@ def plot_log_warnings_and_errors(config_data: configs.Config, config_model: conf
 def _plot_log_warnings_and_errors(filepath_log: str, log_label: str) -> plt.Figure:
     if os.path.exists(filepath_log):
         with open(filepath_log) as file_:
-            lines = [line for line in file_.readlines() if re.search('(warn|error)', line, re.IGNORECASE)]
-        if not lines:
-            lines = ['{} log report:  no lines were found containing obvious warnings or errors'.format(log_label)]
+            raw_lines = [re.sub('\n', '', line) for line in file_.readlines()
+                         if re.search('(warn|error)', line, re.IGNORECASE)]
+        if not raw_lines:
+            raw_lines = ['{} log report:  no lines were found containing obvious warnings or errors'.format(log_label)]
         else:
-            lines.insert(0, '{} log report:  {} lines were found possibly containing warnings or errors'.format(
-                log_label, len(lines)))
+            raw_lines.insert(0, '{} log report:  {} lines were found possibly containing warnings or errors'.format(
+                log_label, len(raw_lines)))
     else:
-        lines = ['{} log report:  no log file was found at {}'.format(log_label, filepath_log)]
-    lines = ''.join(lines)
+        raw_lines = ['{} log report:  no log file was found at {}'.format(log_label, filepath_log)]
+    wrapped_lines = list()
+    for line in raw_lines:
+        wrapped = textwrap.wrap(line, width=_LINE_CHARACTER_LIMIT, subsequent_indent=_LINE_INDENT)
+        wrapped_lines.append('\n'.join(wrapped))
+    finished_lines = '\n\n'.join(wrapped_lines)
     fig, axes = plt.subplots(figsize=(8.5, 11), nrows=1, ncols=1)
-    plt.text(0, 0, lines, **{'fontsize': 8, 'fontfamily': 'monospace'})
+    plt.text(0, 0, finished_lines, **{'fontsize': 8, 'fontfamily': 'monospace'})
     plt.axis('off')
     return fig
